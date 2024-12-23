dc888d73e8760897d91051ae30efc454600e86b3
jcpearlson <jcpearlson@gmail.com>
2024-12-22 21:24:27 -0500

    wip

--- board.py
+++ board.py
@@ -1,5 +1,35 @@
-import numpy as np
+"""
+This will use python-chess module to create a custom chess engine!
+Author: Josh Pearlson
+"""
+import chess
 
-class Board:
+class engine:
+
     def __init__(self):
-        board = np.array[0*10]
+        self.board = chess.Board()
+
+    def eval_function(self, board):
+        # simple eval function that just simply counts white-black pieces
+        white_pieces = sum(len(board.pieces(pt, chess.WHITE)) for pt in chess.PIECE_TYPES)
+        black_pieces = sum(len(board.pieces(pt, chess.BLACK)) for pt in chess.PIECE_TYPES)
+
+        return white_pieces - black_pieces
+
+
+    def iterative_deepening(self,depth=4, alpha=-float('inf'),beta=float('inf')):
+        """
+        This function will use iterative deepening to find the best move for the current board state.
+        """
+        return NotImplemented
+
+    def alpha_beta_search(self, depth, alpha, beta):    
+        """
+        This function will use alpha-beta pruning to find the best move for the current board state.
+        """
+        return NotImplemented
+
+
+if __name__ == "__main__":
+    board = engine()
+
